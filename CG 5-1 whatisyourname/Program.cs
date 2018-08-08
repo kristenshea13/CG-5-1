de8ac006d7553b2ed8a9114158b07a151f01b361
Kristen Shea <kstar61@live.com>
2018-08-07 19:34:27 -0500

    *

--- CG 5-1 whatisyourname/Program.cs
+++ CG 5-1 whatisyourname/Program.cs
@@ -6,7 +6,18 @@ namespace CG_5_1_whatisyourname
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a program that asks the user to enter their first name. Then have it 
+            //    enter their last name.Create a method that combines the first and last 
+            //    name and returns it as a new value.Print the new value.
+
+            Console.WriteLine("What is your name?");
+            Console.WriteLine();
+            Console.Write("Please type your first name: ");
+
+
+
+
+
         }
     }
 }
