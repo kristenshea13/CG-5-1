c482ce4f0d3e2c7ff2592d209c11f09b072a879f
Kristen Shea <kstar61@live.com>
2018-08-07 19:27:17 -0500

    Add project files.

---
+++ CG 5-1 whatisyourname/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_5_1_whatisyourname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
